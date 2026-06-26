6eb5b39106813e42a3e5815bf41ca7674aeb3668
Melwin K S <melwinks75@gmail.com>
2026-06-26 14:10:11 +0530

    homepage rect

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='py-3 my-3'></footer>
+      <hr className='border-bottom'/>
+      <p className='text-light text-center'>&copy; 2026-Built with love ❤️ by Melwin K S</p>
+    </>
+  )
+}
+
+export default Footer
